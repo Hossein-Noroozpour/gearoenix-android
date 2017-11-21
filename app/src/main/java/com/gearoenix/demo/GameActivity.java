e0598b8b8937b6c81ed4f921916a9e79352e14c7
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-11-21 16:26:43 +0330

    Initial commit

---
+++ app/src/main/java/com/gearoenix/demo/GameActivity.java
@@ -0,0 +1,12 @@
+package com.gearoenix.demo;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class GameActivity extends NativeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
