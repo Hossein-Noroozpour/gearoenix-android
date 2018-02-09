2b14fb37cca2941209ede558b60a75c26f865e8d
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2018-02-09 11:47:00 +0330

    android:: opengl:: --recognizing opengl version in native activity, opengl engine can now resume, several events added, test 001 changed a little--

--- app/src/main/java/com/gearoenix/demo/GameActivity.java
+++ app/src/main/java/com/gearoenix/demo/GameActivity.java
@@ -2,11 +2,16 @@ package com.gearoenix.demo;
 
 import android.app.NativeActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameActivity extends NativeActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.screenBrightness = 1.0f;
+        getWindow().setAttributes(params);
     }
 }
